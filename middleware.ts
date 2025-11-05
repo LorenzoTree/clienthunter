35c7cb2292c7b4db6c856e06ea196f4e391b851e
Lorenzo Vegliò <lorenzo.veglio@slu.se>
2025-11-05 18:33:57 +0100

    fix: async auth call for Clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,11 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    const { userId } = auth();
+    const { userId } = await auth();
     if (!userId) {
-      return auth().redirectToSignIn();
+      return (await auth()).redirectToSignIn();
     }
   }
 });
